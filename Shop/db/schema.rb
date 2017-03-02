d874518a4ddfe9e5b22f7c8fa4507c4460695960
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-02 12:29:10 -0800

    generate user model and users controller, add user validations for email and username

--- Shop/db/schema.rb
+++ Shop/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301190009) do
+ActiveRecord::Schema.define(version: 20170302202113) do
 
   create_table "products", force: :cascade do |t|
     t.string   "product_name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20170301190009) do
     t.datetime "updated_at",          null: false
     t.string   "image_url"
     t.integer  "quantity"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
