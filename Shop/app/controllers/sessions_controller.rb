b30992d5b5b1cfde1e3cf2ade036ab20bd7745fb
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-03 13:32:57 -0800

    add #create action for users controller and fix error in #create action for sessions controller

--- Shop/app/controllers/sessions_controller.rb
+++ Shop/app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = user.find_by_email(params[:user][:email])
+    user = User.find_by_email(params[:user][:email])
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
