556a494c640fb9d9546335f6d90465dd25a676d4
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-07 15:51:56 -0800

    add description column to categories table

--- Shop/app/controllers/categories_controller.rb
+++ Shop/app/controllers/categories_controller.rb
@@ -1,36 +1,35 @@
 class CategoriesController < ApplicationController
-before_action :authenticate_admin, except: [:index, :show]
+  before_action :authenticate_admin, except: [:index, :show]
 
-def new
-  @category = Category.new
-end
-
-def create
-  @category = Category.new(category_params)
-  if @category.save
-    flash[:success] = "Category created successfully!"
-    redirect_to @category
-  else
-    render 'new'
-    flash[:error] = "Failed to create category."
+  def new
+    @category = Category.new
   end
-end
 
-def index
-  @categories = Category.all
-end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category created successfully!"
+      redirect_to @category
+    else
+      render 'new'
+      flash[:error] = "Failed to create category."
+    end
+  end
 
-def show
-  @category = Category.all
-  @products = @category.products
-end
+  def index
+    @categories = Category.all
+  end
 
-def destroy
-  @category = Category.find(params[:id])
-  @category.destroy
-  flash[:success] = "Category successfully destroyed!"
-  redirect_to_categories_path
-end
+  def show
+    @category = Category.all
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:success] = "Category successfully destroyed!"
+    redirect_to_categories_path
+  end
 
   private
   def category_params
