4311ec024aa4b3249a35ace9bbeeaffd09c63e68
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-08 10:11:49 -0800

    add form for creating new categories, add a create category link to navbar

--- Shop/db/schema.rb
+++ Shop/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307235107) do
+ActiveRecord::Schema.define(version: 20170308180133) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
+    t.string   "image_url"
   end
 
   create_table "products", force: :cascade do |t|
