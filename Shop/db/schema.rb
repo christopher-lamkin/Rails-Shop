082f26a607666994b77d9f9d226243fa352dad8a
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-08 22:18:13 -0800

    edit categories_product migration

--- Shop/db/schema.rb
+++ Shop/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308180133) do
+ActiveRecord::Schema.define(version: 20170308183936) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170308180133) do
     t.datetime "updated_at",  null: false
     t.string   "description"
     t.string   "image_url"
+  end
+
+  create_table "categories_products", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "products", force: :cascade do |t|
