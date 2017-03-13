3bf38dccdd81a1b53126e0a5e82f52f62a94741f
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-13 16:45:59 -0700

    generate model for orders

--- Shop/app/controllers/carts_products_controller.rb
+++ Shop/app/controllers/carts_products_controller.rb
@@ -6,11 +6,11 @@ class CartsProductsController < ApplicationController
 
   def create
     @user = current_user
-    p "***********************************"
-    p current_user.username
-    p "***********************************"
     @cart = Cart.find_by(user_id: current_user.id)
     @carts_product = CartsProduct.new(cart_id: @cart.id, product_id: params[:product_id], quantity: params[:quantity])
+    p "****************************"
+    p params
+    p "****************************"
     if @carts_product.save
       flash[:success] = "Item added to cart!"
       redirect_to @cart
