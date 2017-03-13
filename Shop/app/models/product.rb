e28bff37dd2d00cab5870d38886ca0fd1c9bdf58
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-13 10:49:37 -0700

    create models for orders, order status, and order items

--- Shop/app/models/product.rb
+++ Shop/app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates_presence_of :product_name, :product_description, :price, :quantity
   has_many :categories_products
   has_many :categories, through: :categories_products
+  has_many :order_items
 end
