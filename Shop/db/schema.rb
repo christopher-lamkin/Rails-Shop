eb6a6b2572dd5021937f0ee0f15c49b98d17182e
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-14 16:27:48 -0700

    create a grid view of categories in index page

--- Shop/db/schema.rb
+++ Shop/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313180308) do
+ActiveRecord::Schema.define(version: 20170313211350) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 20170313180308) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
